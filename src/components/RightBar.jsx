fb00d0f800e746b40f27171ac699d9046c7e8399
Ahmad-Mridul <mridulahmed262@gmail.com>
2026-03-05 19:09:26 +0600

    changes in rightbar

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -4,11 +4,11 @@ import TaskTicketCard from "./TaskTicketCard";
 const RightBar = ({selectedTickets, handleResolvedTask, resolvedTickets}) => {
     return (
         <div className="md:w-[15%]">
-            <p className="text-2xl mb-5">Task Status</p>
+            <p className="text-2xl mb-5 font-bold">Task Status</p>
             {
                 selectedTickets.map(ticket=><TaskTicketCard key={ticket.id} ticket={ticket} handleResolvedTask={handleResolvedTask}/>)
             }
-            <p className="text-2xl mb-5">Resolved Task</p>
+            <p className="text-2xl mb-5 font-bold">Resolved Task</p>
             {
                 resolvedTickets.map(ticket=><ResolvedTask key={ticket.id} ticket={ticket}/>)
             }
